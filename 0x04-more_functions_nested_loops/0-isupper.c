483556d523d68c82db1358d8c26afeb37e9a6570
Notorious <isaacokwuzi@gmail.com>
2022-12-17 11:23:48 -0800

    task 0 correction

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isupper - checks if uppercase
+ *
+ *current mood - lowkey pissed @ Betty
+ *Return: return 1 if uppercase, return 0 if lowercase
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+More Funcs
+fill later
