41536aa0bd577af95b11c2f259303d0567dd04f5
Notorious <isaacokwuzi@gmail.com>
2022-12-18 01:30:12 -0800

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ *mul - multiplier
+ *
+ *@a: int to multiply
+ *
+ *@b: int to multiply
+ *
+ *Return: result of two
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
