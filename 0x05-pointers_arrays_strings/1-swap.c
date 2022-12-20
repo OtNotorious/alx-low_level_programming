c6b30d1436a2cef77332dfd6860bbfbeb71f2d2c
Notorious <isaacokwuzi@gmail.com>
2022-12-20 03:19:55 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps integers
+ *
+ *@a: first integer
+ *@b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+int c = *a;
+int d = *b;
+*a = c;
+*b = d;
+} 
