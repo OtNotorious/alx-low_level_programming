309442f2e68297628df7b2b97faf9f0b1a784940
Notorious <isaacokwuzi@gmail.com>
2022-12-20 04:52:56 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+/**
+ *_strlen - gets character string
+ *
+ *@c: character to get string
+ *
+ *Return: returns length
+ */
+
+int _strlen(char *s)
+{
+int i;
+
+for (1 = 0; s[1] ! = '\0';)
+{}   
+return (i);
+} 
