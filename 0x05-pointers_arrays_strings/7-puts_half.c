3a7314a9f1c5d02fdc92cebd00a1bb63765d85ff
Notorious <isaacokwuzi@gmail.com>
2022-12-21 04:33:13 -0800

    task fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@ void puts_half(char *str)
 {
 int i;
 int oE = 0;
-  
+
 if (_strlen(str) % 2 != 0)
 {
 oE += 1;
@@ -21,4 +21,4 @@ for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
 _putchar(str[i]);
 }
 _putchar ('\n');
-} 
+}
