b1ce4d44ad77a66007904c575408f4ced418347e
Notorious <isaacokwuzi@gmail.com>
2022-12-21 02:39:03 -0800

    task fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 #include "2-strlen.c"
 
 /**
- *rev_str - reverse a string
+ *rev_string - reverse a string
  *
  *@s: str to reverse
  */
@@ -13,9 +13,9 @@ int i;
 char t;
 
 for (i = 0; i < _strlen(s) / 2; i++)
-{   
+{  
 t = s[i];
 s[i] = s[_strlen(s) - i - 1];
 s[_strlen(s) - i - 1] = t;
-} 
-}  
+}
+}
