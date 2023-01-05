52a0750b2a4d4adeac34dcdc908b776f329c0b2d
Notorious <isaacokwuzi@gmail.com>
2023-01-05 11:07:27 -0800

    fixed header file error

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
-#include MAIN_H
-void _putchar(char *s);
+#define MAIN_H
+void _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
