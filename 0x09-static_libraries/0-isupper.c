8832d6bf7ef7f8a29526b63e8b29bdb90c09aa3c
Notorious <isaacokwuzi@gmail.com>
2023-01-06 20:03:13 -0800

    task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isupper - checks if uppercase
+ *
+ *current mood - lowkey pissed @ Betty
+ *@c: character to be checked
+ *Return: return 1 if uppercase, return 0 if lowercase
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
